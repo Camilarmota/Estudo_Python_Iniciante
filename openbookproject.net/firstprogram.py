880e74ee733cdff4bbc773210b72f6539c19262f
Camila <140784690+Camilarmota@users.noreply.github.com>
2024-10-25 13:18:51 -0300

    Update firstprogram.py

--- openbookproject.net/firstprogram.py
+++ openbookproject.net/firstprogram.py
@@ -1,2 +1,4 @@
+## print is a function used to display a value on the screen
+## Quotation marks mark the beginning and end of a value
 print ("Hello, World! This is my first program in Pyhon")
 print (" Friday, October 25th, 2024.")
