6fe03337ee965ec78568924ee110ecc79383308f
Camila <140784690+Camilarmota@users.noreply.github.com>
2024-10-25 14:14:37 -0300

    Update firstprogram.py

--- openbookproject.net/firstprogram.py
+++ openbookproject.net/firstprogram.py
@@ -1,4 +1,7 @@
-## print is a function used to display a value on the screen
-## Quotation marks mark the beginning and end of a value
+## print is a function used to display a value on the screen;
+## Quotation marks mark the beginning and end of a value, a string.
 print ("Hello, World! This is my first program in Pyhon")
 print (" Friday, October 25th, 2024.")
+## The type function can tell you the class of the value, Data type.
+type ("My name is Camila")
+type (2024)
