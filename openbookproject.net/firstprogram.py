a7f6dab301db42a1eb7c03ab03a668fe32a6c1f2
Camila <140784690+Camilarmota@users.noreply.github.com>
2024-10-25 14:28:08 -0300

    Update firstprogram.py

--- openbookproject.net/firstprogram.py
+++ openbookproject.net/firstprogram.py
@@ -5,3 +5,12 @@ print (" Friday, October 25th, 2024.")
 ## The type function can tell you the class of the value, Data type.
 type ("My name is Camila")
 type (2024)
+## Strings belong to the class str.
+## Integers belong to the class int. 
+## Numbers with a decimal point belong to a class called float.
+## Use triple quoted strings when use multiple lines:
+message = """ My name is Camila.
+I'm enjoying studying Python.
+I want to become a data scientist. """
+print (message)
+## message is a variable.
